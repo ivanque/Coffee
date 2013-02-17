c8f3cd91402a4f703e9c86fff7ee20539e428a68
Ivan Yuminov <ivanqueru@gmail.com>
2013-02-17 20:08:21 +0600

    Harlem Shake

---
+++ cappuccino.rb
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+# Wanna play, huh?
+
+def dice
+	die = rand(10)
+	puts "If the number is 6 - you win."
+	puts "Throwing the dice..."
+	if die == 6
+		puts "You won!"
+	else
+		puts "Try again. The number was #{die}."
+	end
+end
+
+dice
+
